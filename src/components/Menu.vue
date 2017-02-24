a71b8b1fa3fa26416c091955f8af359d7e995313
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-02-24 11:19:42 +0100

    Added menu component
    This will eventually render all the menus for the day

---
+++ src/components/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <week-days></week-days>
+  </div>
+</template>
+
+<script>
+import WeekDays from './menu/Days';
+
+export default {
+  components: { WeekDays },
+  name: 'Menu',
+  props: ['day'],
+};
+</script>
