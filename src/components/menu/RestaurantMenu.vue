8c2ad52258225d6d8f1b0826f67856e85a32627a
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-02-24 11:31:33 +0100

    Added RestaurantMenu component
    This will render all the entries for an particular restaurant

---
+++ src/components/menu/RestaurantMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="columns is-multiline">
+    <div v-for="(meal, index) in this.meals" class="column is-6">
+      <div class="box">
+        <div class="content">
+          <div class="level">
+            <h3>{{ meal.name }}</h3>
+            </h2>
+          </div>
+          <meal :meal="meal"></meal>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Meal from './Meal';
+
+export default {
+  components: { Meal },
+  props: {
+    meals: {
+      required: true,
+    },
+  },
+};
+</script>
