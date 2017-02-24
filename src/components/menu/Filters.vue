6a929cbcd10b7e1340ef67beb2abc8a64250398c
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-02-24 15:17:06 +0100

    Fixed menu search and radio buttons on mobile

--- src/components/menu/Filters.vue
+++ src/components/menu/Filters.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="columns">
-    <div class="column is-half is-offset-one-quarter">
-      <div class="columns">
-        <div class="column is-half">
+    <div class="column is-half-widescreen is-offset-one-quarter-widescreen">
+      <div class="columns is-mobile">
+        <div class="column is-half-mobile is-half-tablet">
           <permanent-offer :filter="filter"></permanent-offer>
         </div>
-        <div class="column is-half">
+        <div class="column is-5-mobile is-one-quarter-tablet is-one-quarter-desktop is-half-widescreen">
           <search :filter="filter"></search>
         </div>
       </div>
