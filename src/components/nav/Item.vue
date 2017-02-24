5c982fc2b6e17b99c0fa25d34fccbdd7c0a079fb
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-02-24 11:08:40 +0100

    Added simple navigation components

---
+++ src/components/nav/Item.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link tag="li" :to="item.link" active-class="is-active">
+    <a>{{ item.text }}</a>
+  </router-link>
+</template>
+
+<script>
+import _ from 'lodash';
+
+export default {
+  props: {
+    name: 'nav-item',
+    item: {
+      validator(item) {
+        return _.has(item, 'text') && _.has(item, 'link');
+      },
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/components/nav/Items.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="tabs">
+        <ul>
+            <nav-item v-for="(item, index) in items" :item="item"></nav-item>
+        </ul>
+    </div>
+</template>
+
+<script>
+import NavItem from './Item';
+
+export default {
+  name: 'nav-items',
+  components: { NavItem },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
