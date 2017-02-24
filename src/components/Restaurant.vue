33265c2f7465ccf3b58867075abf473d7ef367a2
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-02-24 12:34:38 +0100

    Added restaurant component

---
+++ src/components/Restaurant.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div v-for="(entries, day) in restaurant.opening_hours" class="column">
+        <hours :label="day" :entries="entries" :highlited="selectedDay(day)"></hours>
+      </div>
+    </div>
+    <restaurant-card :data="restaurant"></restaurant-card>
+  </div>
+</template>
+
+<script>
+import RestaurantService from './../services/Restaurants';
+import RestaurantCard from './restaurant/Restaurant';
+import Hours from './restaurant/Hours';
+import Days from './../core/Days';
+
+export default {
+  components: { RestaurantCard, Hours },
+  name: 'restaurant',
+  props: {
+    slug: {
+      required: true,
+    },
+  },
+  data() {
+    return {
+      restaurant: {},
+    };
+  },
+  methods: {
+    selectedDay(day) {
+      return day.toLowerCase() === Days.getTodayLink(false);
+    },
+    init(slug = null) {
+      let s = slug;
+      if (slug === null) {
+        s = this.slug;
+      }
+
+      this.restaurant = RestaurantService.getRestaurant({ slug: s });
+    },
+  },
+  created() {
+    this.init();
+  },
+  watch: {
+    slug(slug) {
+      this.init(slug);
+    },
+  },
+};
+</script>
