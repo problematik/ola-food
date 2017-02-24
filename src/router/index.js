09ce7de3402ce36f7622c132abd7a9ee2df4956c
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-02-24 11:20:33 +0100

    Added menu routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from 'components/Hello';
+import Menu from 'components/Menu';
+import Days from './../core/Days';
 
 Vue.use(Router);
 
@@ -10,6 +12,24 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+    },
+    {
+      path: '/menu',
+      component: Menu,
+      props: true,
+      children: [
+        {
+          path: '',
+          redirect: Days.getTodayLink(false),
+          component: Menu,
+        },
+        {
+          path: ':day',
+          name: 'menu-day',
+          component: Menu,
+          props: true,
+        },
+      ],
     },
   ],
 });
