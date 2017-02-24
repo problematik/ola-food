2100aacb2c209b985fa3344982ee0bce296e38d7
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-02-24 11:12:58 +0100

    Added days menu component

---
+++ src/components/menu/Days.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav-items
+    :items="items"
+    :class="'is-centered is-medium'">
+  </nav-items>
+</template>
+
+<script>
+import _ from 'lodash';
+import NavItems from '../nav/Items';
+import Days from './../../core/Days';
+
+export default {
+  components: { NavItems },
+  data() {
+    return {
+      items: Days.getWeekLinks(),
+    };
+  },
+  computed: {
+    availableLinks() {
+      return this.items.map(item => item.link);
+    },
+  },
+  methods: {
+    goToCurrentDay() {
+      this.$router.replace(this.items[Days.getDayOfWeek()].link);
+    },
+    assertSupportedPath(path) {
+      if (_.includes(this.availableLinks, path) === false) {
+        this.goToCurrentDay();
+        return false;
+      }
+
+      return true;
+    },
+  },
+  created() {
+    this.assertSupportedPath(this.$route.path);
+  },
+  watch: {
+    $route(to) {
+      this.assertSupportedPath(to.path);
+    },
+  },
+};
+</script>
